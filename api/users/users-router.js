5a00d83b39fcab29564b738fb720051be8655cb0
Robert Gripshover <robertgrips@gmail.com>
2023-04-05 11:26:39 -0400

    building out the users and auth routers and testing as I go

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,8 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
 
 
+const router = require('express').Router()
+
 /**
   [GET] /api/users
 
@@ -24,5 +26,10 @@
   }
  */
 
+router.get('/', (req, res, next) => {
+  res.json('users')
+})
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+
+module.exports = router
